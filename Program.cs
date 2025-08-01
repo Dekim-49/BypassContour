df70407114a65d73da549ee23688b1c1caf225e0
Dekim-49 <chernyadeva.anna@mail.ru>
2025-08-01 12:09:31 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
-﻿namespace BypassContour
+﻿using BypassTheCircuit;
+using System;
+
+namespace BypassContour
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             // false - по часовой
             // true - против часовой
 
             Node tree = Generator.Example();
             //IDrawer drawerDo = new DrawerBitmap(tree, "di_DO");
-            //IDrawer drawerConsol = new DrawerConsole();
+            IDrawer drawerConsol = new DrawerConsole();
             //drawerDo.Draw(tree);
             //drawerConsol.Draw(tree);
             //Console.WriteLine("--------");
